856871fea32b3194ff0062a3bde780701f0fa8a4
Joyeljames <joyeljames333@gmail.com>
2026-02-24 23:28:13 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.1"
+
+REPO_NAME = "Brain_Tumor_detection"
+AUTHOR_USER_NAME = "jOYEL"
+SRC_REPO = "cnn_classification"
+AUTHOR_EMAIL = "joyeljames333@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small package for CNN classification",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    })
